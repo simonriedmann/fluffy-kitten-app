8d409cb83690dd52ef1d9753e1fdebcf6dd9b8c3
simonriedmann <simon.riedmann@web.de>
2021-03-26 09:09:30 +0100

    final version of fluffy-kitten

--- server.js
+++ server.js
@@ -1,42 +1,61 @@
 import express from 'express';
-import mongodb from 'mongodb';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
-const connectionString = 'mongodb://localhost27017';
-const database = 'fluffy-kitten';
+const connectionString = 'mongodb+srv://garfiled:test1234@cluster0.qdwtp.mongodb.net/fluffy-kitten?retryWrites=true&w=majority';
 
-const mongoClient = mongodb.MongoClient;
+mongoose.connect(connectionString)
 
 const server = express();
 
 server.use(bodyParser.json());
 
+const kittySchema = {
+    name: String, fur: String, lives: Number
+}
+
+
+const Cat = mongoose.model('KittyCat', kittySchema)
+//'KittyCat ist wie Schlüssel
+
 server.get('/', (request, response) => {
     response.json({ status: 'Server is up and running.' })
 });
 
+//test ob server
+
 server.get('/cats', (request, response) => {
-    mongoClient.connect(connectionString, async (error, client) => {
-        const db = client.db(databaseName);
-        const kittyCats = await db.collection('kittyCats').find().toArray();
-        response.json(kittyCats);
-    })
+    Cat.find().then((cats) => response.json(cats))
+})
+
+server.get('/cats/:catId', (request, response) => {
+    const catId = request.params.catId;
+    Cat.findById(catId).then(cat => response.json(cat))
 })
 
 server.post('/cats', (request, response) => {
-    const cat = {
+    const kitty = new Cat({
         name: request.body.name,
         fur: request.body.fur,
         lives: request.body.lives
-    }
-
-    mongoClient.connect(connectionString, (error, client)=> {
-        const db = client.db(databaseName);
-        db.collection('kittyCats')
-            .insertOne(cat)
-            .then((result) => response.json(result.ops[0]));
-
     })
-})
+    kitty.save().then(kitty => response.json(`${kitty.name} says meow.`)).catch((error) => response.json(error))
+    })
+
+  server.put('/cats/:catId', (request, response) => {
+        const catId = request.params.catId;
+        const updatedCat = request.body;
+
+        Cat.findOneAndUpdate({ _id: catId }, updatedCat, {
+            new: true,}).then((result) => response.json(result))
+    });
+
+
+    server.delete('/cats/:catId', (request, response) => {
+        const catId = request.params.catId;
+        const updatedCat = request.body;
+
+        Cat.findOneAndDelete({ _id: catId },).then((result) => response.json(result))
+    });
 
 server.listen(4000);
